e664c379b233d366d0e070948df138785f5215e2
danfyfe <fyfedaniel@gmail.com>
2019-10-16 18:14:02 -0400

    keydown movement

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// function moveLegoMan(position){
+//   const legoMan = document.querySelector('#lego-man')
+//   `legoMan.style.${position}` = parseInt(`legoMan.style.${position}`) + 10 + 'px'
+// };
+
 document.addEventListener('DOMContentLoaded', event => {
   const div = document.querySelector('#text-div')
 
-  // button changes H1 and button text, then adds WOO! every time you click the button
+  // button changes H1 and button text, then adds WOO! every time you click the button after the first time
   const button = document.querySelector('#btn')
   button.addEventListener('click', event => {
     if (div.innerText === 'YOU CLICKED ME!' || div.innerText.includes(' WOO!')) {
@@ -14,11 +19,33 @@ document.addEventListener('DOMContentLoaded', event => {
 
   const imageContainer = document.querySelector('#img-container')
   const legoMan = document.createElement('img')
+  legoMan.id = 'lego-man'
   legoMan.alt = 'lego man'
   legoMan.src = 'https://randomuser.me/api/portraits/lego/6.jpg'
 
   document.addEventListener('keydown', event => {
-    
+
+    switch (event.key) {
+      case " ":
+        imageContainer.appendChild(legoMan)
+        legoMan.style.top = 0
+        legoMan.style.left = 0
+        break;
+      case "ArrowUp":
+        legoMan.style.top = parseInt(legoMan.style.top) - 10 + 'px'
+        break;
+      case "ArrowLeft":
+        legoMan.style.left = parseInt(legoMan.style.left) - 10 + 'px'
+        break;
+      case "ArrowDown":
+        legoMan.style.top = parseInt(legoMan.style.top) + 10 + 'px'
+        break;
+      case "ArrowRight":
+        legoMan.style.left = parseInt(legoMan.style.left) + 10 + 'px'
+        break;
+      default:
+        null
+    }
   })
 
 
